b8f66baa2a33821dc37b0a23f411b19b08945aec
Lucas Bianchezzi Oliveira <41365718875@info.local>
2024-05-14 14:23:30 -0300

    Projeto finalizado (resta verificação)

--- API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ API-VitalHub/WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd15974cb35f4d58b45cc2a81e814f7ad924a4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03eb036b4e060b659d6078a844a0bcf64c5ebb93")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
