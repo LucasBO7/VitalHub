e230b63b579471bb1ca9634dbe7a4680d2722a47
Lucas Bianchezzi Oliveira <lucasbianchezzi700@gmail.com>
2025-01-17 19:44:37 -0300

    Merge da branch develop para main com remoção das APIs de backup

--- API-VitalHub-recente-desatualizada-backup/WebAPI/WebAPI/Utils/Mail/IEmailService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebAPI.Utils.Mail
-{
-    public interface IEmailService
-    {
-        //Método assíncrono para envio de email
-        Task SendEmailAsync(MailRequest mailRequest);
-    }
-}
